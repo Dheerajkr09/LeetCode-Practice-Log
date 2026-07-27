d6a58d6285ffee7757970c531c4c0fc6e9fc0d0f
Dheeraj <nishukumar75321@gmail.com>
2026-07-27 22:48:13 +0530

    Time: 31 ms (19.8%), Space: 27.1 MB (22.94%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,5 +1,22 @@
 class Solution {
 public:
+
+    string generate(string& word)
+    {
+        int hash[26] = {0};
+        
+        for(char& ch:word)
+        {
+            hash[ch-'a']++;
+        }
+        string new_word = "";
+        for(int i = 0;i < 26;i++)
+        {
+            int count = hash[i];
+            new_word += string(count, i+'a');
+        }
+        return new_word;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
 
         int n = strs.size();
@@ -9,17 +26,22 @@ public:
 
         for(int i = 0;i < n;i++)
         {
-            string temp = strs[i];
-            sort(temp.begin(),temp.end());
-            mp[temp].push_back(strs[i]);
+            string word = strs[i];
+
+            string new_word = generate(word);
+
+            mp[new_word].push_back(strs[i]);
+
         }
 
-        for(auto it: mp)
+        for(auto it:mp)
         {
             result.push_back(it.second);
         }
 
         return result;
+
+        
 
 
 
